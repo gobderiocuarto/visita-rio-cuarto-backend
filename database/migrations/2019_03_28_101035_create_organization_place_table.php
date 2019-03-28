34c26bb1013e8377a75b3924f7776b8f317327f9
Guillermo <copydot@gmail.com>
2019-03-28 07:12:01 -0300

    Crecion de tabla / migracion organization_place

---
+++ database/migrations/2019_03_28_101035_create_organization_place_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationPlaceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_place', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('organization_id')->unsigned();
+            $table->integer('place_id')->unsigned();
+            
+            $table->timestamps();
+
+            //relation
+            $table->foreign('organization_id')->references('id')->on('organizations')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('place_id')->references('id')->on('places')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_place');
+    }
+}
