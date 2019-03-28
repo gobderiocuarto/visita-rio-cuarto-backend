3c68d614fe22f22d5d1ba2a27d530fa349e2fc4d
Guillermo <copydot@gmail.com>
2019-03-28 19:06:30 -0300

    Agregado de CategoryStoreRequest, metodo CategoryController::store

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\CategoryStoreRequest;
+
+use App\Category;
+
 class CategoryController extends Controller
 {
 
@@ -45,10 +49,16 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        dd($request);
+        //dd($request);
+
+        $category = Category::create($request->all());
+
+        return redirect()->route('admin.categories.edit', $category->id)->with('info', 'Categoría creada con éxito');
     }
+
+
 
     /**
      * Display the specified resource.
--- app/Http/Requests/CategoryStoreRequest.php
+++ app/Http/Requests/CategoryStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CategoryStore extends FormRequest
+class CategoryStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class CategoryStore extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class CategoryStore extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
         ];
     }
 }
