3939a8ef53638ef6f6b97fc7d375d2d63604b055
Guillermo <copydot@gmail.com>
2019-07-24 12:19:10 -0300

    Quitar target en nav front / admin

--- resources/views/admin/layouts/partials/nav_bar.blade.php
+++ resources/views/admin/layouts/partials/nav_bar.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <a class="navbar-brand" href="{{ url('/') }}" target="_blank">
+    <a class="navbar-brand" href="{{ url('/') }}">
         {{ config('app.name', 'Laravel') }}
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
