352727788895ef188a24ddeea2dccdc7fbc1dbbb
cmblue <german.sayago@gmail.com>
2019-11-04 10:31:51 -0300

    update config/database.php

--- config/database.php
+++ config/database.php
@@ -1,17 +1,5 @@
 <?php
 
-// localhost database
-define('DB_HOST', env('DB_HOST', '127.0.0.1'));
-define('DB_USERNAME', env('DB_USERNAME', 'forge'));
-define('DB_PASSWORD', env('DB_PASSWORD', 'forge'));
-define('DB_DATABASE', env('DB_DATABASE', ''));
-
-// aws RDS
-// define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
-// define('DB_USERNAME', $_SERVER['RDS_USERNAME']);
-// define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
-// define('DB_DATABASE', $_SERVER['RDS_DB_NAME']);
-
 return [
 
     /*
@@ -54,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => DB_HOST,
+            'host' => 'DB_HOST', env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => DB_DATABASE,
-            'username' => DB_USERNAME,
-            'password' => DB_PASSWORD,
+            'database' => 'DB_DATABASE', env('DB_DATABASE', ''),
+            'username' => 'DB_USERNAME', env('DB_USERNAME', 'forge'),
+            'password' => 'DB_PASSWORD', env('DB_PASSWORD', 'forge'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
