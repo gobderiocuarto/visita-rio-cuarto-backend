2111cc2d9a4f4227bedce7f474169c0ef8757a97
Guillermo Rodriguez <copydot@gmail.com>
2019-11-09 10:27:26 -0300

    Footer: Cambio en leyenda, acortandola temporalmente para evitar problemas de interlineado en pantallas de pequeño tamaño

--- resources/views/web/layouts/partials/footer.blade.php
+++ resources/views/web/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="container">
-    El portal <a href="http://visitariocuarto.gob.ar">Visita Río Cuarto</a> es un proyecto desarrollado por el <a href="http://riocuarto.gov.ar" target="_blank">Gobierno de Río Cuarto</a>
+    Desarrollado por el <a href="http://riocuarto.gov.ar" target="_blank">Gobierno de Río Cuarto</a>
   </div>
 </footer>
