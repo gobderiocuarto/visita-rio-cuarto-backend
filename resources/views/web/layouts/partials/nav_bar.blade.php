30de266b9f24d192708e0fb7265c6137201467a4
Guillermo Rodriguez <copydot@gmail.com>
2019-11-13 11:31:19 -0300

    Agregado de buscador en NavBar, creación de metodo en controlador Eventos

--- resources/views/web/layouts/partials/nav_bar.blade.php
+++ resources/views/web/layouts/partials/nav_bar.blade.php
@@ -6,10 +6,8 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav mr-auto ml-sm-3">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             ¿Qué Hacer?
@@ -32,6 +30,10 @@
           </div>
         </li>
       </ul>
+      <form action="{{url('/eventos') }}" class="form-inline my-2 my-lg-0" method="GET">
+        <input type="text" class="form-control mr-sm-2" type="search" placeholder="Buscar" name="busqueda"  required="required" minlength=3 >
+        <button type="submit" class="btn btn-outline-light my-2 my-sm-0">Buscar</button>
+      </form>
     </div>
   </div>
 </nav>
