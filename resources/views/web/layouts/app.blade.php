1d1564605ff9e83264ace039374c643dcc53b665
Guillermo Rodriguez <copydot@gmail.com>
2019-11-14 09:24:30 -0300

    Agregado de script google analytics entre head en app.blade.php

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -6,6 +6,15 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-20054145-10"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-20054145-10');
+    </script>
     <link rel="stylesheet" href="{{ asset('css/main.min.css') }}">
 </head>
 <body>
