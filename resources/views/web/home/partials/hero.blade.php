42b059faf15275bcd810fd975f648e420e87d9f0
Guillermo Rodriguez <copydot@gmail.com>
2019-11-15 11:45:02 -0300

    Front :: Hero: Agregado de opción de busqueda mediante boton-select de 'Donde'

--- resources/views/web/home/partials/hero.blade.php
+++ resources/views/web/home/partials/hero.blade.php
@@ -1,10 +1,10 @@
 <div class="hero">
   <div class="container">
     <div class="row justify-content-md-center">
-      <div class="col col-md-4">
+      <div class="col col-md-3">
         <div class="dropdown">
           <button class="btn btn-block btn-lg btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            ¿Qué querés hacer?
+            ¿Qué hacer?
           </button>
           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
              @foreach ($event_tags as $tag )
@@ -13,16 +13,30 @@
           </div>
         </div>
       </div>
-      <div class="col col-md-4">
+      <div class="col col-md-3">
         <div class="dropdown">
           <button class="btn btn-block btn-lg btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            ¿Cuándo lo querés hacer?
+            ¿Cuándo?
           </button>
           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <a class="dropdown-item" href="{{ url('eventos/cuando/hoy') }}">Hoy</a>
             <a class="dropdown-item" href="{{ url('eventos/cuando/maniana') }}">Mañana</a>
             <a class="dropdown-item" href="{{ url('eventos/cuando/fin-de-semana') }}">Este fin de semana</a>
             <a class="dropdown-item" href="{{ url('eventos/cuando/mes') }}">Este Mes</a>
+          </div>
+        </div>
+      </div>
+      <div class="col col-md-3">
+        <div class="dropdown">
+          <button class="btn btn-block btn-lg btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ¿Dónde?
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="{{ url('eventos/donde/17/resto-bares') }}">Resto Bares</a>
+            <a class="dropdown-item" href="{{ url('eventos/donde/56/discotecas-pubs') }}">Discotecas – Pubs</a>
+            <a class="dropdown-item" href="{{ url('eventos/donde/28/museos-y-centros-culturales') }}">Museos y Centros Culturales</a>
+            <a class="dropdown-item" href="{{ url('eventos/donde/33/paseos-al-aire-libre') }}">Paseos al aire libre</a>
+            <a class="dropdown-item" href="{{ url('eventos/donde/27/teatros') }}">Teatros</a>
           </div>
         </div>
       </div>
