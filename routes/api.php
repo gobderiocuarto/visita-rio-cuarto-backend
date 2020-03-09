59a222982f984d91d462b5f2b9e4887798d60bd1
Guillermo Rodriguez <GHRodriguez@mriv.local>
2020-03-09 09:50:56 -0300

    Modificacion de Event resource y routes Api,  creacion de Api Event Controller

--- routes/api.php
+++ routes/api.php
@@ -13,45 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::apiResource('categoriesAll', 'Api\CategoryController');
-Route::apiResource('organizationsAll', 'Api\OrganizationController');
-Route::apiResource('eventsAll', 'Api\EventController');
+Route::resource('events', 'Api\EventController')->only(['index', 'show' ]);
 
-
-//
-// Api Categories
-//
-use App\Category;
-use App\Http\Resources\Category as CategoryResource;
-Route::get('/categories/{id}', function ($id) {
-    return new CategoryResource(Category::find($id) ?? abort(404));
-});
-Route::get('/categories', function () {
-  return CategoryResource::collection(Category::paginate());
-});
-
-
-//
-// Api Organizations
-//
-use App\Organization;
-use App\Http\Resources\Organization as OrganizationResource;
-Route::get('/organizations/{id}', function ($id) {
-    return new OrganizationResource(Organization::find($id) ?? abort(404));
-});
-Route::get('/organizations', function () {
-  return OrganizationResource::collection(Organization::paginate());
-});
-
-
-//
-// Api Events
-//
-use App\Event;
-use App\Http\Resources\Event as EventResource;
-Route::get('/events/{id}', function ($id) {
-    return new EventResource(Event::find($id) ?? abort(404));
-});
-Route::get('/events', function () {
-  return EventResource::collection(Event::paginate());
-});
+// Route::apiResource('categoriesAll', 'Api\CategoryController');
+// Route::apiResource('organizationsAll', 'Api\OrganizationController');
+// Route::apiResource('eventsAll', 'Api\EventController');
