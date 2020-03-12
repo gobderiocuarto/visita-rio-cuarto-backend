79e65edfb623ef5df68ddab7b6d3d5c0f0a5097f
Guillermo Rodriguez <dg.guillermo.rodriguez@gmail.com>
2020-03-12 13:46:28 -0300

    Agregado de parametros de url al query para devolver al paginado de registros

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -16,9 +16,14 @@ class ServiceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        # coleccion general de organizaciones
+        $organization = $this->getBaseCollection();
+
+        $organization = $this->getQueries($request, $organization);
+
+        return OrganizationResource::collection($organization);
     }
 
     /**
@@ -83,5 +88,65 @@ class ServiceController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+
+    /*--------------------------------------/*
+    /*---------------------------------------
+        Metodos privados
+    /*---------------------------------------
+    /*--------------------------------------*/
+
+    private function getBaseCollection()
+    {
+        $organization = Organization::where('organizations.state', 1)
+                                    ->select('organizations.*');
+        
+        # Orden de presentación
+        $organization = $organization->orderBy('organizations.name', 'ASC');
+
+        return $organization;
+
+    }
+
+
+    private function getQueries(Request $request, $organizations)
+    {
+        # Paginar registros, valor por defecto
+        $paginate = 12; 
+
+        # Almacenar los query a la url para mantenerlos en paginado de la consulta
+        $query_filter = (object)[];
+
+        # Filtrar por campo busqueda
+        if (($request->search != '')) {
+            $organizations = $organizations->where('organizations.name', 'like', '%'.$request->search.'%' );
+            $query_filter->search = $request->search;
+        }
+
+        # Filtrar por categoría (id)
+        if (((int)$request->category != 0 )) {
+            $organizations = $organizations->join ('categories','categories.id','organizations.category_id')
+            ->where(function ($query) use ($request) {
+                $query  ->where ('categories.id', '=', $request->category)
+                        ->orWhere ('categories.category_id', '=', $request->category);
+            });
+
+            $query_filter->category = $request->category;
+        }
+
+        # Paginar registros
+        if ($request->paginate != '') {
+            $paginate = $request->paginate;
+
+            $query_filter->paginate = $request->paginate;
+        } 
+        $organizations = $organizations->paginate($paginate);
+
+        # Agregar los query de la url al paginado de la consulta
+        $organizations->appends((array)$query_filter);
+
+        return $organizations;
     }
 }
